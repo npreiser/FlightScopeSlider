cb9d5548f4841e4e35c8353d59bc98812ef42171
Nick Preiser <nickpreiser@gmail.com>
2022-01-17 18:08:22 -0800

    dev work

--- trinamic/trinamic_pg.c
+++ trinamic/trinamic_pg.c
@@ -7,14 +7,30 @@
 // Include the IC(s) you want to use
 #include "TMC-API/tmc/ic/TMC5160/TMC5160.h"
 
-#define MOTOR0			0 // Motor 0
+#define MOTOR0 0 // Motor 0
 #define MAX_VEL 600000
 #define MAX_ACC 5000
 
+int vstart = 0;
+int a1 = 100;
+int v1 = 100;
+int amax = 5000; // 1000;
+int vmax = 5000; // 3000;
+int dmax = 4700;
+int d1 = 1400;
+int vstop = 10;
+float revs = 1; // 60; //0.5;
+
+#define MAX_REVOLUTIONS 20			 // this will be based on testing of system...
+#define NUMBER_OF_REGIONS 5			 //
+#define TOTAL_WIDTH_STEPS 51200 * 10 // this should be tuned per length of raild..
+
 void resetMotorDrivers(char motor);
 
-void waitFor(char motor, int position) {
+void waitFor(char motor, int position)
+{
 	int pos = tmc5160_readInt(motor, TMC5160_XACTUAL);
+
 	while (pos != position)
 	{
 		//delay(100);
@@ -22,19 +38,33 @@ void waitFor(char motor, int position) {
 	}
 }
 
-int bcminit() {
-	if (!bcm2835_init()) {
+void waitForVelocity0(char motor)
+{
+	int velocity = tmc5160_readInt(motor, TMC5160_VACTUAL);
+	while (velocity > 0)
+	{
+		velocity = tmc5160_readInt(motor, TMC5160_VACTUAL);
+	}
+	//printf("velocity is now 0\n");
+}
+
+int bcminit()
+{
+	if (!bcm2835_init())
+	{
 		printf("bcm2835_init() failed");
 		exit(-1);
 	}
 	return 0;
 }
 
-int initwirepi() {
+int initwirepi()
+{
 	wiringPiSetupGpio();
 }
 
-void initSpi() {
+void initSpi()
+{
 	// Initiate SPI
 	bcm2835_spi_begin();
 	bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);	// MSB first
@@ -44,7 +74,8 @@ void initSpi() {
 	bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);	// set CS polarity to low
 }
 
-int setupGpio() {
+int setupGpio()
+{
 	bcminit();
 
 	initwirepi();
@@ -62,7 +93,8 @@ int setupGpio() {
 	return 0;
 }
 
-void configureMotor(char motor) {
+void configureMotor(char motor)
+{
 	resetMotorDrivers(motor);
 
 	// MULTISTEP_FILT=1, EN_PWM_MODE=1 enables stealthChop™
@@ -81,16 +113,6 @@ void configureMotor(char motor) {
 	tmc5160_writeInt(motor, TMC5160_TPWMTHRS, 0x000001F4);
 }
 
-int vstart = 0;
-int a1 = 100;
-int v1 = 100 ;
-int amax = 5000; // 1000;
-int vmax = 500000; // 3000;   
-int dmax = 4700;
-int d1 = 1400;
-int vstop = 10;
-float revs = 60; //0.5;
-
 void initMotorParams(char motor)
 {
 	// Values for speed and acceleration
@@ -98,35 +120,56 @@ void initMotorParams(char motor)
 	tmc5160_writeInt(motor, TMC5160_A1, a1);
 	tmc5160_writeInt(motor, TMC5160_V1, v1);
 	tmc5160_writeInt(motor, TMC5160_AMAX, amax);
-	tmc5160_writeInt(motor, TMC5160_VMAX, vmax);
+	tmc5160_writeInt(motor, TMC5160_VMAX, 200000);
 	tmc5160_writeInt(motor, TMC5160_DMAX, dmax);
 	tmc5160_writeInt(motor, TMC5160_D1, d1);
 	tmc5160_writeInt(motor, TMC5160_VSTOP, vstop);
 	tmc5160_writeInt(motor, TMC5160_RAMPMODE, TMC5160_MODE_POSITION);
+	tmc5160_writeInt(motor, TMC5160_SWMODE, 0x03);
+}
 
-    tmc5160_writeInt(motor, TMC5160_SWMODE, 0x03);
-
-	//TMC5160_XLATCH
-	// for homeing ,  set a 0 into exlatch,  then slowly go home... direction... 
-	// 
+void initMotorParamsHoming(char motor)
+{
+	// Values for speed and acceleration
+	tmc5160_writeInt(motor, TMC5160_VSTART, vstart);
+	tmc5160_writeInt(motor, TMC5160_A1, a1);
+	tmc5160_writeInt(motor, TMC5160_V1, v1);
+	tmc5160_writeInt(motor, TMC5160_AMAX, amax);
+	tmc5160_writeInt(motor, TMC5160_VMAX, 20000);
+	tmc5160_writeInt(motor, TMC5160_DMAX, dmax);
+	tmc5160_writeInt(motor, TMC5160_D1, d1);
+	tmc5160_writeInt(motor, TMC5160_VSTOP, vstop);
+	tmc5160_writeInt(motor, TMC5160_RAMPMODE, TMC5160_MODE_POSITION);
+	tmc5160_writeInt(motor, TMC5160_SWMODE, 0x90A); //enable  right, and flip polarity, for pull up. set xlatch up
+	tmc5160_writeInt(motor, TMC5160_XLATCH, 0x00);	// set xlatch as point 0
 }
 
 void gotoTarget(char motor, int targetPos, int wait)
 {
 	tmc5160_writeInt(motor, TMC5160_XTARGET, targetPos);
-	if (wait) {
+	if (wait)
+	{
 		waitFor(motor, targetPos);
 	}
 }
 
-void disableMotorPower() {
+void gotoTargetHome(char motor, int targetPos)
+{
+	tmc5160_writeInt(motor, TMC5160_XTARGET, targetPos);
+	delay(100);
+	waitForVelocity0(motor);
+}
+
+void disableMotorPower()
+{
 	digitalWrite(4, HIGH); // Disable driver stage
 	delay(10);
-	digitalWrite(2, LOW);  // Remove VCC_IO voltage from Motor 0
+	digitalWrite(2, LOW); // Remove VCC_IO voltage from Motor 0
 	delay(10);
 }
 
-void shutdown() {
+void shutdown()
+{
 	// End SPI communication
 	bcm2835_spi_end();
 	bcm2835_close();
@@ -148,7 +191,8 @@ void resetMotorDrivers(char motor)
 	}
 }
 
-int tune_max_speed() {
+int tune_max_speed()
+{
 	if (setupGpio() == 1)
 	{
 		return 1;
@@ -160,16 +204,15 @@ int tune_max_speed() {
 
 int run_example()
 {
-	if (setupGpio() == 1) {
-      return 1;
+	if (setupGpio() == 1)
+	{
+		return 1;
 	}
 	configureMotor(MOTOR0);
 
 	initMotorParams(MOTOR0);
 
-	//waitFor(0, 10);
-
-	int targetPos = (int)(revs * 51200.0);     ///  NOTE  51200 is the number of microsteps per rev. 
+	int targetPos = (int)(revs * 51200.0); ///  NOTE  51200 is the number of microsteps per rev.
 	gotoTarget(MOTOR0, targetPos, 1);
 	printf("TMC5160 Position: %d\n", tmc5160_readInt(MOTOR0, TMC5160_XACTUAL));
 
@@ -185,10 +228,106 @@ int run_example()
 	return 0;
 }
 
-int return_pass() {
+// public routines start
+// ******************************************************
+int initDriver()
+{
+	printf("TMC5160 Driver init\n");
+
+	if (setupGpio() == 1)
+	{
+		return 1;
+	}
+	configureMotor(MOTOR0);
 	return 0;
 }
 
-void main() {
-	run_example();
+int cleanupDriver()
+{
+	printf("TMC5160 Driver cleanup\n");
+
+	disableMotorPower();
+	shutdown();
+	return 0;
+}
+
+int setNormalRunModeParams()
+{
+	initMotorParams(MOTOR0);
+}
+
+// will find right most postion,  and set that as 0...
+int findHome()
+{
+
+	initMotorParamsHoming(MOTOR0);
+	int targetPos = (int)(MAX_REVOLUTIONS * 51200.0); ///  NOTE  51200 is the number of microsteps per rev.
+
+	gotoTargetHome(MOTOR0, targetPos); // go right until velocity == 0,
+
+	printf("TMC5160 Position: %d\n", tmc5160_readInt(MOTOR0, TMC5160_XACTUAL));
+	printf("TMC5160 Xlatch: %d\n", tmc5160_readInt(MOTOR0, TMC5160_XLATCH));
+	int diff = tmc5160_readInt(MOTOR0, TMC5160_XACTUAL) - tmc5160_readInt(MOTOR0, TMC5160_XLATCH);
+	printf("diff calc : %d\n", diff);
+	tmc5160_writeInt(MOTOR0, TMC5160_XACTUAL, diff); //set new home position
+	gotoTarget(MOTOR0, 0, 1);						 //go back to home postion that was just set..
+
+	printf("FINAL HOME TMC5160 Position: %d\n", tmc5160_readInt(MOTOR0, TMC5160_XACTUAL));
+
+	delay(500);
+	return 0;
+}
+
+// testing code,
+// assume total width is dified up 5 positions 0, 1, 2,3,4.. 0 = home,  2 center, 4 left most region.
+//
+void gotoRegion(int targetRegion)
+{
+	//initMotorParams(MOTOR0);
+	// 0 based region number  .
+	int tpos = 0;
+	if (targetRegion > 0)
+	{
+		int region = targetRegion;
+		if (region > NUMBER_OF_REGIONS - 1) // never let it get above the max amount.
+			region = NUMBER_OF_REGIONS - 1;
+
+		// total width in # of steps .
+		int stepsPerRegion = TOTAL_WIDTH_STEPS / NUMBER_OF_REGIONS;
+
+		tpos = -1 * (stepsPerRegion * region);
+	}
+
+    printf("going to target position: %d\n", tpos);
+
+	tmc5160_writeInt(MOTOR0, TMC5160_XTARGET, tpos);
+	waitFor(MOTOR0, tpos); // wait for position. to be found.
+	printf("TMC5160 Position: %d\n", tmc5160_readInt(MOTOR0, TMC5160_XACTUAL));
+}
+
+int wiggle_test()
+{
+	setNormalRunModeParams();
+	printf("running wiggle test \n");
+	int targetPos = (int)(3 * 51200.0);
+	gotoRegion(4);
+	printf("TMC5160 Position: %d\n", tmc5160_readInt(MOTOR0, TMC5160_XACTUAL));
+	delay(2000);
+	gotoRegion(0);
+}
+
+void main()
+{
+	initDriver();
+	setNormalRunModeParams();
+	printf("Init TMC5160 Position: %d\n", tmc5160_readInt(MOTOR0, TMC5160_XACTUAL));
+
+
+    gotoRegion(2);
+	//findHome();
+	//delay(2000);
+	//gotoRegion(1);
+	//wiggle_test();
+
+	cleanupDriver();
 }
